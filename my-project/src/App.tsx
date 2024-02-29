67b1f55277609012cf7487c9a18f366ecfb79fda
jingjingRain <1982439813@qq.com>
2024-02-29 17:51:10 +0800

    fix test

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider,Button } from '@mantine/core';
 
-function App() {
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+function Demo() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   );
 }
+const App = () => {
+  return (
+    <div>
+      <Button
+        style={{
+          backgroundColor: "blue",
+          color: "white",
+          padding: "10px 20px",
+          borderRadius: "5px",
+        }}
+      >
+        Click me
+      </Button>
+    </div>
+  );
+};
+const CompA = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>CompA</h2>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <CompB count={count} />
+    </div>
+  );
+};
+interface IProps {
+  count: number
+}
+class CompB extends Component<IProps, any> {
+  render() {
+    return <h1>Count from CompA{this.props.count}</h1>
+  }
+}
 
-export default App;
+export default CompA;
